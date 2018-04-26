ce6354158112939f8479215a64faa28d92a97170
David <xodnjs2549@naver.com>
2018-04-26 17:11:10 +0900

    Refactor : 테스트 수정

--- src/test/java/view/InputViewTest.java
+++ src/test/java/view/InputViewTest.java
@@ -9,7 +9,7 @@ import static org.junit.Assert.assertEquals;
 public class InputViewTest {
     @Test
     public void userCount() {
-        Scanner scanner = new Scanner("2");
-        assertEquals(2, InputView.getUserCount(scanner));
+        Scanner scanner = new Scanner("3");
+        assertEquals(3, InputView.getUserCount(scanner));
     }
 }
